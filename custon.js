fa512677ab32de806fab871e49550a772e4a5c2d
ArthurPantoja <arthurcristian.180@gmail.com>
2022-12-20 09:54:20 -0300

    Versão do dia 20/12

---
+++ custon.js
@@ -0,0 +1,15 @@
+function alerta(){
+    alert ("Você está sendo direcionado para pagina HTML!")
+}
+
+function alertahome(){
+    alert ("Você está sendo direcionado a pagina Home!")
+}
+
+function alertacss(){
+    alert("Você está sendo direcionado para pagina CSS!")
+}
+
+function alertajava(){
+    alert("Você está sendo direcionado para pagina JavaScript!")
+}
